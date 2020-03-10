082246e2c8077efb929cfe93900c6a2144ef9064
ferb00000100 <fhlh@protonmail.com>
2020-03-09 19:20:00 -0600

    added build folder

---
+++ client/build/precache-manifest.5a501bb7c31968a6ab377ed5d5f61539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95bb5c9703d0714e634f",
+    "url": "/static/js/main.8ab9202c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fa0945cc99d901a1f93",
+    "url": "/static/css/2.de1b1de4.chunk.css"
+  },
+  {
+    "revision": "2fa0945cc99d901a1f93",
+    "url": "/static/js/2.effeffc2.chunk.js"
+  },
+  {
+    "revision": "2549cf49a100ee56aa63898e42b4409e",
+    "url": "/static/media/dms_flow.2549cf49.png"
+  },
+  {
+    "revision": "074a7d63a6003bb43b228162879bcb54",
+    "url": "/static/media/S3_icon.074a7d63.png"
+  },
+  {
+    "revision": "1a9c06994a054456345ca501382f903c",
+    "url": "/index.html"
+  }
+];
