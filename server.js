f94f4a9d151a4cae5c86174bbef18241d89054a5
ferb00000100 <fhlh@protonmail.com>
2020-03-09 19:30:00 -0600

    reordered routes for heroku

--- server.js
+++ server.js
@@ -10,17 +10,21 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Add routes
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
-// Add routes
-app.use(routes);
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Connect to the Mongo DB Local
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dmsdb");
